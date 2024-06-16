fe6926a9aa78908b4d4ce18d0e2a94f12b9b3c9f
simran <simrangurdasani05@gmail.com>
2024-06-16 20:58:55 +0530

    updated

--- src/app/template-driven/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven/template-driven-form/template-driven-form.component.ts
@@ -7,7 +7,24 @@ import { Component } from '@angular/core';
 })
 export class TemplateDrivenFormComponent {
 
-  submitForm(registerForm:any){
-    console.log(registerForm)
+  // defaultSelectedValue="CMPN"
+
+  form = {
+    'username':'',
+    'email':'',
+    'gender':'',
+    'address':'',
+    'branch':'CMPN'
   }
+
+  submitForm(){
+    // if it is one way data binding [ngModel] this defaultSelectedValue did not change but when we did two way data binding,this value changes to what we selected
+    // console.log(this.defaultSelectedValue)
+    // console.log(registerForm)
+    console.log(this.form)
+  }
+
+  // if i want to give defaultValues i will use banana in a box
+  
+
 }
